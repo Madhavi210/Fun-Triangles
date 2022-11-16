676272b6c83a84cea13d9dfc69dced26cb21ae5c
Madhavi210 <jmadhavi156@gmail.com>
2022-11-16 23:11:19 +0530

    update

--- triangle_quize.js
+++ triangle_quize.js
@@ -1,3 +1,5 @@
 var formQuize = document.querySelector(".quize-form");
 var submitBtn = document.querySelector(".submit-btn");
 var output = document.querySelector(".output");
+
+const answers = ['55°', "Isosceles", " 4 cm", "BF = CE"];
