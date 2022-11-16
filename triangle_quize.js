079870131c2ad59b7d2524a2f51fc79f0c6d5f6c
Madhavi210 <jmadhavi156@gmail.com>
2022-11-16 23:11:43 +0530

    update

--- triangle_quize.js
+++ triangle_quize.js
@@ -3,3 +3,7 @@ var submitBtn = document.querySelector(".submit-btn");
 var output = document.querySelector(".output");
 
 const answers = ['55°', "Isosceles", " 4 cm", "BF = CE"];
+
+function calculateScore() {
+    let score = 0;
+    let index = 0;
