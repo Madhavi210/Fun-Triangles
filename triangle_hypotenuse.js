795312973e5a596f35bf3a15cd8e2542dd4988c2
Madhavi210 <jmadhavi156@gmail.com>
2022-11-29 12:31:55 +0530

    updated

--- triangle_hypotenuse.js
+++ triangle_hypotenuse.js
@@ -2,7 +2,6 @@ const length = document.querySelectorAll(".length");
 const calculateBtn = document.querySelector(".calculate-btn");
 const lengthHypotenuse = document.querySelector(".result");
 
-
 let hypotenuse;
 function calculateHypotenuse() {
 
